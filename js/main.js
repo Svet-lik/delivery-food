0390b2332aec2de39f5667a33222146b45d54e0b
Svet-lik <64750023+Svet-lik@users.noreply.github.com>
2020-10-27 15:27:13 +0300

    Make Login

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 const cartButton = document.querySelector("#cart-button");
 const modal = document.querySelector(".modal");
 const close = document.querySelector(".close");
+const userName = document.querySelector(".user-name");
+const buttonOut = document.querySelector(".button-out");
 
 cartButton.addEventListener("click", toggleModal);
 close.addEventListener("click", toggleModal);
@@ -8,3 +10,68 @@ close.addEventListener("click", toggleModal);
 function toggleModal() {
   modal.classList.toggle("is-open");
 }
+
+// Day 1
+
+const buttonAuth = document.querySelector(".button-auth");
+const modalAuth = document.querySelector(".modal-auth");
+const closeAuth = document.querySelector(".close-auth");
+const logInForm = document.querySelector("#logInForm");
+const loginInput = document.querySelector("#login");
+
+let login = localStorage.getItem("cloDelivery");
+
+function toggleModalAuth() {
+  modalAuth.classList.toggle("is-open");
+}
+
+function autorized() {
+  function logOut() {
+    login = null;
+    localStorage.removeItem("gloDelivery");
+    buttonAuth.style.display = "";
+    userName.style.display = "";
+    buttonOut.style.display = "";
+    checkAuth();
+    buttonOut.removeEventListener("click", logOut);
+  }
+  userName.textContent = login;
+  buttonAuth.style.display = "none";
+  userName.style.display = "inline";
+  buttonOut.style.display = "block";
+  buttonOut.addEventListener("click", logOut);
+}
+
+function notAutorized() {
+  function logIn(event) {
+    event.preventDefault();
+    login = loginInput.value;
+
+    if (login) {
+      localStorage.setItem("gloDelivery", login);
+
+      toggleModalAuth();
+      checkAuth();
+      buttonAuth.removeEventListener("click", toggleModalAuth);
+      closeAuth.removeEventListener("click", toggleModalAuth);
+      logInForm.removeEventListener("submit", logIn);
+      logInForm.reset();
+    } else {
+      alert("Логин не введён");
+    }
+  }
+
+  buttonAuth.addEventListener("click", toggleModalAuth);
+  closeAuth.addEventListener("click", toggleModalAuth);
+  logInForm.addEventListener("submit", logIn);
+}
+
+function checkAuth() {
+  if (login) {
+    autorized();
+  } else {
+    notAutorized();
+  }
+}
+
+checkAuth();
