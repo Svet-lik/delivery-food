b53ea83c4fdebd0b54224f0d202aa5a829e9e612
Svet-lik <64750023+Svet-lik@users.noreply.github.com>
2020-10-27 11:13:01 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
-const cartButton = document.querySelector('#cart-button');
-const modal = document.querySelector('.modal');
-const close = document.querySelector('.close');
+const cartButton = document.querySelector("#cart-button");
+const modal = document.querySelector(".modal");
+const close = document.querySelector(".close");
 
-const toggleModal = () => {
-  modal.classList.toggle('is-open');
+cartButton.addEventListener("click", toggleModal);
+close.addEventListener("click", toggleModal);
+
+function toggleModal() {
+  modal.classList.toggle("is-open");
 }
-
-cartButton.addEventListener('click', toggleModal);
-close.addEventListener('click', toggleModal);
-
-new WOW().init();
